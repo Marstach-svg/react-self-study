53eb118cdee4eb0f74e5004148cff5f6bc4c3e74
Marstach <ryoken.0325@gmail.com>
2026-01-31 19:56:35 +0900

    画面土台作成

--- raretech-react-todo/src/App.jsx
+++ raretech-react-todo/src/App.jsx
@@ -3,7 +3,27 @@ import './App.css';
 export const App = () => {
   return (
     <div className="App">
-      
+      <h1>TodoList</h1>
+      {/* Todo追加領域 */}
+      <section>
+        <h2>Add TODO</h2>
+        <input type="text" />
+      </section>
+
+      {/* Todo一覧表示領域 */}
+      <section>
+        <ul>
+          <li>
+            <span>タスク1</span>
+            <i className="fa-solid fa-trash-can"></i>
+          </li>
+          <li>
+            <span>タスク2</span>
+            <i className="fa-solid fa-trash-can"></i>
+          </li>
+        </ul>
+      </section>
+
     </div>
   );
 };
