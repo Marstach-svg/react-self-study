c8434600026a5cfde4993d195c4c59573e29dcc4
Marstach <ryoken.0325@gmail.com>
2026-02-15 16:54:11 +0900

    todoの追加機能実装（初期リストはハードコードなのでDBなし->毎回リセット）

--- raretech-react-todo/src/App.jsx
+++ raretech-react-todo/src/App.jsx
@@ -16,13 +16,31 @@ const initTodo = [
 
 export const App = () => {
   const [todoList, setTodoList] = React.useState(initTodo);
+  const [addInputValue, setAddInputValue] = React.useState("");
+  const [uniqueId, setUniqueId] = React.useState(initTodo.length);
+
+
+  const onChangeAddInputValue = (event) => {
+    setAddInputValue(event.target.value);
+  }
+
+  const handleAddTodo = (event) => {
+    if (event.key==="Enter" && addInputValue!=="") {
+      const newUniqueId = uniqueId + 1;
+      setUniqueId(newUniqueId);
+      const newTodoList = [...todoList, {id: uniqueId, title: addInputValue}];
+      setTodoList(newTodoList);
+      setAddInputValue("");
+    }
+  }
+
   return (
     <div className="App">
       <h1 className="title">TodoList</h1>
       {/* Todo追加領域 */}
       <section className='common-area'>
         <h2 className="add-title">Add TODO</h2>
-        <input type="text" />
+        <input type="text" value={addInputValue} onChange={onChangeAddInputValue} onKeyDown={handleAddTodo} />
       </section>
 
       {/* Todo一覧表示領域 */}
